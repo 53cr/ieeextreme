f673d40e9252d1a1883490afcee0864b09866e96
Burke Libbey <burke@burkelibbey.org>
2009-10-23 18:53:24 -0500

    blah

---
+++ 10/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    Scanner in = new Scanner(System.in);
+    int cases = in.nextInt();
+    String data;
+    
+    for (int i=0; i<cases; ++i) {
+      data = in.nextLine();
+      
+      
+    }
+    
+  }
+}
